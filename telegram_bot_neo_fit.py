712038e3a763e1321421ef32e26eb5e51a61741d
Sergey Chernyshov <thesaren117@yandex.ru>
2022-08-30 20:24:14 +0300

    Выборка занятий для взрослых и только групповой зал

--- telegram_bot_neo_fit.py
+++ telegram_bot_neo_fit.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+import re
 import telebot
 from telebot import types
 
@@ -29,25 +30,34 @@ def start_bot():
     @bot.message_handler(commands=['/schedule'])
     def get_data(message):
         options = Options()
-        options.add_argument('--no-sandbox')
+
         options.headless = True
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())
                                   , options=options)
 
         bot.send_message(message.chat.id, 'Звоню в неофит')
-
+        ## Открываем страницу NEO-FIT
         driver.get('https://s.n-fit.ru/?link=')
-
         sleep(3)
+        ### Выбираем только групповые занятия
+        button_group = driver.find_element(By.XPATH, '/html/body/div[8]/div/section[1]/a[2]')
+        button_group.click()
+        ### Выбираем только занятия для взрослых
+        button_adult_timetable = driver.find_element(By.XPATH, '//*[@id="timetable"]/div/header/div[1]/a[1]')
+        button_adult_timetable.click()
+        ### Раскрываем всё расписание для парсинга
         button_more_timetable = driver.find_element(By.ID, 'show-more-lessons')
         button_more_timetable.click()
-
+        ### Запаковывакм расписание в переменную в переменную
         schedule = driver.find_elements(By.CLASS_NAME, 'timetableEntries')
 
+        ### Проходимся по ней итератором
         for i in schedule:
             data = i.text
-
-        bot.send_message(message.chat.id, data)
+            ### Удаляем не нужные слова
+            result = re.sub('Групповой зал,  ', '', data)
+        ### Отправляем сообщение с расписанием
+        bot.send_message(message.chat.id, result)
 
         driver.close()
         driver.quit()
@@ -60,4 +70,4 @@ def start_bot():
         get_data(message)
 
 
-    bot.polling()
+    bot.infinity_polling(timeout=5, skip_pending=True)
